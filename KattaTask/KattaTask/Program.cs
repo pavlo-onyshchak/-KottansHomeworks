ae471215f4983ce9ec8c7d3504fe7acb33ecb297
pavlo-onyshchak <36053219+pavlo-onyshchak@users.noreply.github.com>
2018-12-09 18:23:23 +0200

    katta

---
+++ KattaTask/KattaTask/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KattaTask
+{
+    public class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            string[] delimiters = new string[] { ",", "\n" };
+            string[] digits = numbers.Split(delimiters, StringSplitOptions.None);
+
+            if (numbers.StartsWith("//"))
+            {
+                string[] brackets = { "]", "[" };
+                int index = numbers.IndexOf('\n');
+                int length = index - 2;
+                var only_delimiters = numbers.Substring(2, length);
+
+                delimiters = only_delimiters.Split(brackets, StringSplitOptions.RemoveEmptyEntries);
+                string without_delimiters = numbers.Substring(index + 1); //string after [];
+                digits = without_delimiters.Split(delimiters, StringSplitOptions.None);
+            }
+
+            int sum = 0;
+            List<string> not_valid_numbers = new List<string>();
+
+            foreach (var element in digits)
+            {
+                int value = Int32.Parse(element);
+                if (value < 0)
+                {
+                    not_valid_numbers.Add(element);
+                    continue;
+                }
+
+                if (value > 1000)
+                {
+                    continue;
+                }
+
+                sum += value;
+            }
+
+            if (not_valid_numbers.Count > 0)
+            {
+                throw new ArgumentException("negatives not allowed " + string.Join(",", not_valid_numbers));
+            }
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add("-1,2"));
+        }
+    }
+}
---
+++ KattaTask/KattaTaskTests/KattaTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KattaTask;
+
+namespace KattaTaskTests
+{
+    [TestClass]
+    public class KattaTests
+    {
+        [TestMethod]
+        public void calculator_take_0_numbers()
+        {
+             Assert.AreEqual(Calculator.Add(""), 0);
+        }
+
+        [TestMethod]
+        public void calculator_take_1_numbers()
+        {
+            Assert.AreEqual(Calculator.Add("1"), 1);
+        }
+
+        [TestMethod]
+        public void calculator_take_2_numbers()
+        {
+            Assert.AreEqual(Calculator.Add("1,2"), 3);
+        }
+
+        [TestMethod]
+         public void calculator_take_an_unknown_amount_of_numbersnumbers()
+        {
+            Assert.AreEqual(Calculator.Add("1,2,3,20,4"), 30);
+        }
+        [TestMethod]
+        public void calculator_handle_new_lines_between_numbers()
+        {
+            Assert.AreEqual(Calculator.Add("1\n2,3"), 6);
+        }
+        [TestMethod]
+        public void two_delimiter_together()
+        {
+            try
+            {
+                Calculator.Add("1,\n");
+                Assert.Fail();
+            }
+            catch (FormatException e)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void calculator_change_delimiters()
+        {
+            Assert.AreEqual(Calculator.Add("//;\n1;2"), 3);
+        }
+
+        [TestMethod]
+        public void calculator_take_negative_numbers()
+        {
+            try
+            {
+                Calculator.Add("-1,2");
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                string expected = "negatives not allowed -1";
+                Assert.AreEqual(expected, e.Message);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void calculator_take_numkbers_bigger_than_1000()
+        {
+            Assert.AreEqual(Calculator.Add("2,1001"), 2);
+        }
+
+        [TestMethod]
+        public void calculator_take_delimiters_of_any_length()
+        {
+            Assert.AreEqual(Calculator.Add("//[***]\n1***2***3"), 6);
+        }
+
+        [TestMethod]
+        public void calculator_take_multiple_delimiters()
+        {
+            Assert.AreEqual(Calculator.Add("//[*][%]\n1*2%3"), 6);
+        }
+    }
+}
