3a260079273eba8afa93a41ce3d210532773d63c
pavlo-onyshchak <36053219+pavlo-onyshchak@users.noreply.github.com>
2018-12-09 18:19:03 +0200

    add the unit tests

---
+++ KottansTasks/KottansTasks.Tests/KottansTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KottansTasks.Tests
+{
+    [TestClass]
+    public class KottansTests
+    {
+        [TestMethod]
+        public void Test_GenerateNextCreditCard_function()
+        {
+            //arrange
+            string number = "358999999999999992";
+            CreditCard card = new CreditCard(number);
+            
+            string card_number;
+            //act
+            try
+            {
+                card_number = card.GenerateNext(number);
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual("no more card numbers available for this vendor", ex.Message);
+            }
+
+            //assert
+           
+        }
+        [TestMethod]
+         
+        public void Test_GetVendor_function()
+        {
+            //arrange
+            string number = "4539145406914866";
+            bool actual;
+            bool expected = true;
+
+            //act
+            CreditCard card = new CreditCard(number);
+            actual = card.IsValid(number);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+    }
+}
--- KottansTasks/KottansTasks/Program.cs
+++ KottansTasks/KottansTasks/Program.cs
@@ -2,7 +2,7 @@
 
 namespace KottansTasks
 {
-    class CreditCard
+   public class CreditCard
     {
         private string card_id;
         public CreditCard(string val_of_card_id)
@@ -14,14 +14,22 @@ namespace KottansTasks
         public string GenerateNext(string card_number)
         {
             string name_of_vendor = GetVendor(card_number);
-            int size = card_number.Length;
+            string without_space = card_number.Replace(" ", "");
+            int size = without_space.Length;
             int[] arr = new int[size-1];
             Random r = new Random();
             string next_number_card=null;
+            int[] arr_tmp = new int[size];
+            arr_tmp = ConvertToInt(card_number);
 
             switch (name_of_vendor)
             {
                 case "JCB":
+                     if (arr_tmp[2] == 8 && arr_tmp[3] == 9)
+                    {
+                        throw new ArgumentException("no more card numbers available for this vendor");
+                    }
+
                     arr[0] = 3;
                     arr[1] = 5;
                     for(int i = 2; i<size-1; i++)
@@ -49,16 +57,46 @@ namespace KottansTasks
                     break;
 
                 case "Visa":
+                    int sum_expected_1 = (size - 2) * 9;
+                    int sum_actual_1 = 0;
+                  
+                    arr_tmp = ConvertToInt(card_number);
+
+                    for (int i = 1; i < size - 1; i++)
+                    {
+                        sum_actual_1 += arr_tmp[i];
+                    }
+                    
+                    if(sum_actual_1 == sum_expected_1)
+                    {
+                        throw new ArgumentException("no more card numbers available for this vendor");
+                    }
+
                     arr[0] = 4;
-                    for(int i = 1; i<size-1; i++ )
+                    for(int i = 1; i < size-1; i++ )
                     {
                         arr[i] = r.Next(0, 9);
                     }
                     break;
 
                 case "American express":
-                    arr[0] = 3;
+                    if (arr_tmp[1] == 7)
+                    {
+                        int sum_expected = (size - 3) * 9;
+                        int sum_actual = 0;
 
+                        for (int i = 2; i < size - 1; i++)
+                        {
+                            sum_actual += arr_tmp[i];
+                        }
+
+                        if (sum_actual == sum_expected)
+                        {
+                            throw new ArgumentException("no more card numbers available for this vendor");
+                        }
+                    }
+
+                    arr[0] = 3;
                     if(card_id[1] == '4')
                     {
                         arr[1] = 7;
@@ -75,6 +113,21 @@ namespace KottansTasks
                     break;
 
                 case "Maestro":
+
+                    if (arr_tmp[0] == 6 || arr_tmp[1] == 7)
+                    {
+                        int sum_expected = (size - 3) * 9;
+                        int sum_actual = 0;
+                        for (int i = 2; i < size - 1; i++)
+                        {
+                            sum_actual += arr_tmp[i];
+                        }
+
+                        if (sum_actual == sum_expected)
+                        {
+                            throw new ArgumentException("no more card numbers available for this vendor");
+                        }
+                    }
                     arr[0] = r.Next(5, 6);
 
                     if(arr[0] == 5)
@@ -119,7 +172,21 @@ namespace KottansTasks
 
                 case "Master Card":
 
-                    if(card_id[0] == '2')
+                    if (arr_tmp[0] == 5 && arr_tmp[1] == 5)
+                    {
+                        int sum_expected = (size - 3) * 9;
+                        int sum_actual = 0;
+                        for (int i = 2; i < size - 1; i++)
+                        {
+                            sum_actual += arr_tmp[i];
+                        }
+
+                        if (sum_actual == sum_expected)
+                        {
+                            throw new ArgumentException("no more card numbers available for this vendor");
+                        }
+                    }
+                    if (card_id[0] == '2')
                     {
                         arr[0] = 5;
                         arr[1] = r.Next(1, 5);
@@ -197,43 +264,78 @@ namespace KottansTasks
 
             return tmp; 
         }
+        private bool LengthIsValid(string card_number)
+        {
+            string name_of_vendor = GetVendor(card_number);
+            string without_space = card_number.Replace(" ", "");
+            int size = without_space.Length;
+            bool is_valid = false;
+            switch (name_of_vendor)
+            {
+                case "JCB":
+                    if (size >= 16 && size <= 19)
+                    {
+                        is_valid = true;
+                    }
+                        break;
+                case "Visa":
+                    if (size == 13 || size == 16 || size == 19)
+                    {
+                        is_valid = true;
+                    }
+                        break;
+                case "American express":
+                    if (size == 15)
+                    {
+                        is_valid = true;
+                    }
 
-
+                        break;
+                case "Maestro":
+                    if(size >= 12 && size <= 19)
+                    {
+                        is_valid = true;
+                    }
+                    break;
+                case "Master Card":
+                    if (size == 16)
+                    {
+                        is_valid = false;
+                    }
+                    break;
+            }
+            return is_valid;
+        }
+            
         public bool IsValid(string card_number)
         {
-
-
-            string tmp = card_number.Replace(" ", "");
-            int size = tmp.Length;
-
-            if (size < 12 || size > 19)
+            bool is_valid = LengthIsValid(card_number);
+            if (is_valid)
             {
-                return false;
-            }
-
-
-            int[] arr = new int[size];
-            int sum_of_digits = 0;
-            arr = ConvertToInt(tmp);
-
-            for (int i = size - 2; i >= 0; i = i - 2)
-            {
-                arr[i] *= 2;
-                if (arr[i] > 9)
+                string without_space = card_number.Replace(" ", "");
+                int size = without_space.Length;
+                int[] arr = new int[size];
+                int sum_of_digits = 0;
+                arr = ConvertToInt(without_space);
+                for (int i = size - 2; i >= 0; i = i - 2)
                 {
-                    arr[i] = arr[i] - 9;
+                    arr[i] *= 2;
+                    if (arr[i] > 9)
+                    {
+                        arr[i] = arr[i] - 9;
+                    }
                 }
-            }
 
-            for (int i = 0; i < size; i++)
-            {
-                sum_of_digits += arr[i];
-            }
-            sum_of_digits *= 9;
+                for (int i = 0; i < size; i++)
+                {
+                    sum_of_digits += arr[i];
+                }
+                sum_of_digits *= 9;
 
-            if (sum_of_digits % 10 == 0)
-            {
-                return true;
+                if (sum_of_digits % 10 == 0)
+                {
+                    return true;
+                }
             }
             return false;
         }
@@ -250,8 +352,8 @@ namespace KottansTasks
         public string GetVendor(string card_number)
         {
             string tmp = "Unknown";
-
-            int size = card_number.Length;
+            string without_space = card_number.Replace(" ", "");
+            int size = without_space.Length;
             
             char tmp1 = card_number[0];
             switch (tmp1)
@@ -273,7 +375,7 @@ namespace KottansTasks
                     break;
 
                 case '3':
-                    if (size < 16 && size > 19)
+                    if (size < 16 || size > 19)
                     {
                         tmp = "The length of you card is invalid";
                         return tmp;
@@ -292,6 +394,12 @@ namespace KottansTasks
                     }
                     else
                     {
+                        if(size != 15)
+                        {
+                            tmp = "The length of you card is invalid";
+                            return tmp;
+                        }
+
                         if (card_number[1] == '4' || card_number[1] == '7')
                         {
                             tmp = "American express";
@@ -300,17 +408,17 @@ namespace KottansTasks
                     break;
 
                 case '4':
-                    if (size != 16)
+                    if (size == 13 || size == 16 || size == 19)
                     {
-                        tmp = "The length of you card is invalid";
+                        tmp = "Visa";
                         return tmp;
                     }
 
-                    tmp = "Visa";
+                    tmp = "The length of you card is invalid";
                     break;
 
                 case '5':
-                    if (size < 12 && size > 19)
+                    if (size < 12 || size > 19)
                     {
                         tmp = "The length of you card is invalid";
                         return tmp;
@@ -340,7 +448,7 @@ namespace KottansTasks
                     break;
 
                 case '6':
-                    if (size < 12 && size > 19)
+                    if (size < 12 || size > 19)
                     {
                         tmp = "The length of you card is invalid";
                         return tmp;
@@ -373,15 +481,15 @@ namespace KottansTasks
     {
         static void Main()
         {
-            string number = "4026883624516838";
+            string number = "358999999999999992";
             CreditCard card = new CreditCard(number);
         
-            string next_number = card.GenerateNext(number);
-            string name_of_vendor = card.GetVendor(number);
-            bool is_valid = card.IsValid(number);
-            Console.WriteLine(name_of_vendor);
-            Console.WriteLine(is_valid);
-            Console.WriteLine(next_number);
-         }
+           string next_number = card.GenerateNext(number);
+           // string name_of_vendor = card.GetVendor(number);
+           //bool is_valid = card.IsValid(number);
+            //Console.WriteLine(name_of_vendor);
+          //  Console.WriteLine(is_valid);
+             Console.WriteLine(next_number);
+        }
     }
 }
